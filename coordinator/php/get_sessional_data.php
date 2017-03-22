8a950fb17ac1ec5f02211cee2ad20a20818730ee
ILYAS <ILYAS@ILYAS>
2017-03-22 20:28:58 +0530

    modified dashboard

--- coordinator/php/get_sessional_data.php
+++ coordinator/php/get_sessional_data.php
@@ -89,7 +89,7 @@
                             print('{"data":'.json_encode($result).'}');
                         } else {
                             echo '{"fieldErrors":[{"name":"'.$column.'","status":"failed to updated."}],"data":[]}';
-                        }
+                            }
 
                     
                 //}
@@ -130,7 +130,95 @@
             }*/
         }
 
-    }
+    }else if($_POST['action'] == "create"){
+
+         $data = $_POST['data'][0];
+
+            $enroll = $data['enroll'];
+
+            $name = $data['name'];
+
+            $course = $data['course'];
+
+            $semester = $data['semester'];
+
+            $section = $data['section'];
+
+            $marks_sub1 = $data['marks_sub1'];
+
+            $marks_sub2 = $data['marks_sub2'];
+
+            $marks_sub3 = $data['marks_sub3'];
+
+            $marks_sub4 = $data['marks_sub4'];
+
+            $marks_sub5 = $data['marks_sub5'];
+
+            $marks_sub6 = $data['marks_sub6'];
+
+            $total_marks = $data['total_marks'];
+
+
+
+            $sql = "INSERT INTO sessional_awards (enroll, name, course, semester, section, address, email, reg_no, batch, reg_status) VALUES ('$enroll', '$name', '$course', '$semester', '$section', '$address', '$email', '$reg_no', '$batch', '$reg_status')";
+
+
+            if ($conn->query($sql) === TRUE) {
+
+                $query = "SELECT * from teacher_profile WHERE teacher_id = $teacher_id";
+
+
+                $res = $conn -> query($query);
+
+                $result = array();
+
+
+                while ($row = $res -> fetch_assoc()) {
+                    # code...
+                    $result[] = $row;
+
+                }
+
+                print('{"data":'.json_encode($result).'}');
+            }    
+            else
+            {
+                echo '{"fieldErrors":[{"name":"coordinating_course","status":"Please select different."},{"name":"coordinating_sec","status":"Please select different."},{"name":"coordinating_sem","status":"Please select different."}],"data":[]}';
+            }    
+        }
+        else if ($_POST['action'] == "remove")
+        {
+
+            $data = $_POST['data'];
+
+            $id = array_keys($data);
+    
+            $id = array_shift($id);
+
+            $delete_sql = "UPDATE teacher_profile SET type = 'normal', coordinating_course = NULL,  coordinating_sem = NULL,  coordinating_sec = NULL WHERE teacher_id = $id";
+
+            if ($conn->query($delete_sql) === TRUE) {
+
+                $query = "SELECT * from teacher_profile";
+
+                $res = $conn -> query($query);
+
+
+                $result = array();
+
+
+                while ($row = $res -> fetch_assoc()) {
+                    # code...
+                    $result[] = $row;
+
+                }
+
+                print('{"data":'.json_encode($result).'}');
+
+
+            }
+
+        }
     else
     {
 
